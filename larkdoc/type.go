68f1358a4d8a6c42aa51dd1d1c308b195d1c52d3
kongdefei <kongdefei@bytedance.com>
2023-09-03 16:45:16 +0800

    fix type missing for docv12

--- larkdoc/type.go
+++ larkdoc/type.go
@@ -2,6 +2,97 @@ package larkdoc
 
 import (
 	doc "github.com/larksuite/oapi-sdk-go/service/doc/v2"
+	larkdocx "github.com/larksuite/oapi-sdk-go/v3/service/docx/v1"
+	larkdrive "github.com/larksuite/oapi-sdk-go/v3/service/drive/v1"
+)
+
+// 1	页面 Block
+// 2	文本 Block
+// 3	标题 1 Block
+// 4	标题 2 Block
+// 5	标题 3 Block
+// 6	标题 4 Block
+// 7	标题 5 Block
+// 8	标题 6 Block
+// 9	标题 7 Block
+// 10	标题 8 Block
+// 11	标题 9 Block
+// 12	无序列表 Block
+// 13	有序列表 Block
+// 14	代码块 Block
+// 15	引用 Block
+// 17	待办事项 Block
+// 18	多维表格 Block
+// 19	高亮块 Block
+// 20	会话卡片 Block
+// 21	流程图 & UML Block
+// 22	分割线 Block
+// 23	文件 Block
+// 24	分栏 Block
+// 25	分栏列 Block
+// 26	内嵌 Block Block
+// 27	图片 Block
+// 28	开放平台小组件 Block
+// 29	思维笔记 Block
+// 30	电子表格 Block
+// 31	表格 Block
+// 32	表格单元格 Block
+// 33	视图 Block
+// 34	引用容器 Block
+// 35	任务 Block
+// 36	OKR Block
+// 37	OKR Objective Block
+// 38	OKR Key Result Block
+// 39	OKR Progress Block
+// 40	新版文档小组件 Block
+// 41	Jira 问题 Block
+// 42	Wiki 子目录 Block
+// 999	未支持 Block
+type DocxBlockType int
+
+const (
+	DocxBlockTypePage = iota + 1
+	DocxBlockTypeText
+	DocxBlockTypeHeading1
+	DocxBlockTypeHeading2
+	DocxBlockTypeHeading3
+	DocxBlockTypeHeading4
+	DocxBlockTypeHeading5
+	DocxBlockTypeHeading6
+	DocxBlockTypeHeading7
+	DocxBlockTypeHeading8
+	DocxBlockTypeHeading9
+	DocxBlockTypeBullet
+	DocxBlockTypeOrdered
+	DocxBlockTypeCode
+	DocxBlockTypeQuote
+	DocxBlockTypeTodo
+	DocxBlockTypeBitable
+	DocxBlockTypeCallout
+	DocxBlockTypeDiagram
+	DocxBlockTypeDivider
+	DocxBlockTypeFile
+	DocxBlockTypeGrid
+	DocxBlockTypeGridColumn
+	DocxBlockTypeIframe
+	DocxBlockTypeImag
+	DocxBlockTypeISV
+	DocxBlockTypeMindnote
+	DocxBlockTypeSheet
+	DocxBlockTypeTable
+	DocxBlockTypeTableCell
+	DocxBlockTypeView
+	DocxBlockTypeQuoteContainer
+	DocxBlockTypeTask
+	DocxBlockTypeOKR
+	DocxBlockTypeObjective
+	DocxBlockTypeKeyResult
+	DocxBlockTypeProgress
+	DocxBlockTypeNewDocBlock
+	DocxBlockTypeJira
+	DocxBlockTypeWikiSubDir
+
+	DocxBlockTypeUndefined = 999
 )
 
 type BlockType string
@@ -153,8 +244,16 @@ type DocContent struct {
 	Body     DocBody  `json:"body"`
 }
 
+type DocContentV2 struct {
+	Blocks []*larkdocx.Block `json:"blocks,omitempty"`
+}
+
 type DocMeta struct {
 	*doc.DocMetaResult
+}
+
+type DocMetaV2 struct {
+	*larkdrive.Meta
 }
 
 type DocRawContent struct {
